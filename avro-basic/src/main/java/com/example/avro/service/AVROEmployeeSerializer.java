eec3569099b39b8e39d9461f75511638860e8e14
Jatish Khanna <jatish.khanna@gmail.com>
2019-03-16 14:56:16 +0530

    Update AVROEmployeeSerializer.java

--- avro-basic/src/main/java/com/example/avro/service/AVROEmployeeSerializer.java
+++ avro-basic/src/main/java/com/example/avro/service/AVROEmployeeSerializer.java
@@ -1,24 +1,40 @@
 package com.example.avro.service;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
 
-public class AVROEmployeeSerializer {
-	
-  public void serialize() throws Exception {
-    Schema schema = new Schema.Parser().parse(new File("employee.avsc"));
-	
-	GenericRecord firstEmployee = new GenericData.Record(schema);
-	
-	firstEmployee.put("name","Jatish Khanna");
-	firstEmployee.put("id", 1001);
-	firstEmployee.put("company", "Infosys Ltd.");
-	
-	DatumWriter<GenericRecord> writer = new SpecificDatumWriter<>(schema);
-	
-	DataFileWriter<GenericRecord> fileWriter = new DataFileWriter<>(writer);
-	
-	fileWriter.create(schema, new File("employee.avro"));
-	fileWriter.append(firstEmployee);
-	fileWriter.close();
-  }
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+
+
+public class AVROEmployeeSerializer extends AVROEmployeeServiceBase {
+	public void serialize() throws Exception {
+		GenericRecord firstEmployee = new GenericData.Record(schema);
+		GenericRecord firstAddress = new GenericData.Record(schema.getField("address").schema());
+
+		String[] name = { "Jatish", "Khanna" };
+		firstEmployee.put("name", Arrays.asList(name));
+		firstEmployee.put("employeeCertifications", new HashMap<>());
+		firstEmployee.put("id", 1001);
+		firstEmployee.put("intField", 0);
+		firstEmployee.put("longField", 0l);
+
+		firstAddress.put("flatNo", 11);
+		firstAddress.put("pincode", 126101l);
+
+		firstEmployee.put("address", firstAddress);
+
+		DatumWriter<GenericRecord> writer = new SpecificDatumWriter<>(schema);
+
+		DataFileWriter<GenericRecord> fileWriter = new DataFileWriter<>(writer);
+
+		fileWriter.create(schema, new File("employee.avro"));
+		fileWriter.append(firstEmployee);
+		fileWriter.close();
+	}
 }
