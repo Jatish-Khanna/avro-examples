2c2493923bf88b63e1ce72eaa0976e5bbfd87ad3
Jatish Khanna <jatish.khanna@gmail.com>
2019-03-16 14:46:40 +0530

    Update AVROEmployeeDeSerializer.java

--- avro-basic/src/main/java/com/example/avro/service/AVROEmployeeDeSerializer.java
+++ avro-basic/src/main/java/com/example/avro/service/AVROEmployeeDeSerializer.java
@@ -2,28 +2,25 @@ package com.example.avro.service;
 
 import java.io.File;
 
-import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.specific.SpecificDatumReader;
 
-public class AVROEmployeeDeSerializer {
+public class AVROEmployeeDeSerializer extends AVROEmployeeServiceBase {
 
-	public void deserialize() throws Exception {
-    Schema schema = new Schema.Parser().parse(new File("employee.avsc"));
-		
-	DatumReader<GenericRecord> reader = new SpecificDatumReader<>(schema);
-	
-	DataFileReader<GenericRecord> fileReader = new DataFileReader<>(new File("employee.avro"), reader);
-	
-	GenericRecord employee = null;
-	
-	while(fileReader.hasNext()) {
-		employee = fileReader.next(employee);
-		System.out.println("Employee Info: "+ employee);
-	}
-	
-	fileReader.close();
+ public void deserialize() throws Exception {
+  DatumReader<GenericRecord> reader = new SpecificDatumReader<>(schema);
+
+  DataFileReader<GenericRecord> fileReader = new DataFileReader<>(new File("employee.avro"), reader);
+
+  GenericRecord employee = null;
+
+  while (fileReader.hasNext()) {
+   employee = fileReader.next(employee);
+   System.out.println("Employee Info: " + employee);
   }
+
+  fileReader.close();
+ }
 }
